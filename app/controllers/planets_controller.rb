8815074f2a52ff54ded49c163c3f526fd0bf74e7
LEETAEHO <tae1560@gmail.com>
2013-02-06 16:39:07 +0900

    Optimize

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -322,7 +322,7 @@ class PlanetsController < ApplicationController
     @planet.destroy
 
     respond_to do |format|
-      format.html { redirect_to :back }
+      format.html { render :json => "Success" }
       format.json { head :no_content }
     end
   end
